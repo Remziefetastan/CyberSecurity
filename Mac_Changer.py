82bef83ba242f0b87ee308d9bea02f06d3cfda1f
Remziefetastan <127629375+Remziefetastan@users.noreply.github.com>
2024-09-19 17:52:16 +0300

    Update Mac_Changer.py

--- Mac_Changer.py
+++ Mac_Changer.py
@@ -1,14 +1,11 @@
 import subprocess
 
-# İşletim sistemindeki ağ arabirimini almak için sistem komutunu kullanın
-interface = input("Hangi ağ arabirimini kullanmak istersiniz? (Örneğin: eth0, wlan0) ")
+interface = input("Which network interface do you want to use? (Example: eth0, wlan0) ")
 
-# Yeni MAC adresini al
-new_mac = input("Yeni MAC adresini girin (Örneğin: 00:11:22:33:44:55): ")
+new_mac = input("Enter new MAC adress (Example: 00:11:22:33:44:55): ")
 
-# Yeni MAC adresini değiştirin
-print("Yeni MAC adresi: ", new_mac)
+print("New MAC adress: ", new_mac)
 subprocess.call(["sudo", "ifconfig", interface, "down"])
 subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
 subprocess.call(["sudo", "ifconfig", interface, "up"])
-print("MAC adresi değiştirildi!")
+print("MAC address has been changed!")
