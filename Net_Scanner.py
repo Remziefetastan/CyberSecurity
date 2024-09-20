6fdbe737a073837fcf46f0cc9814075df5a67944
Remziefetastan <127629375+Remziefetastan@users.noreply.github.com>
2024-09-20 17:54:38 +0300

    Delete Net_Scanner.py

--- Net_Scanner.py
+++
@@ -1,7 +0,0 @@
-import scapy.all as scapy
-
-arp_request_packet = scapy.ARP(pdst="10.0.2.1/24")
-broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-combined_packet = broadcast_packet/arp_request_packet
-(answered_list , unanswered_list) = scapy.srp(combined_packet,timeout=1)
-answered_list.summary()
